75f58c426dd7682bdafe3d151ea347d28003e08b
ketul2212 <pketul2212@gmail.com>
2022-07-11 10:49:41 +0530

    Lombok

--- src/main/java/com/ketul/module/User.java
+++ src/main/java/com/ketul/module/User.java
@@ -23,34 +23,4 @@ public class User {
 	
 	@NotBlank(message = "email can not be null")
 	private String email;
-	
-	public User() {
-		
-	}
-	
-	public User(String name, String email) {
-		this.name = name;
-		this.email = email;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	@Override
-	public String toString() {
-		return "User [name=" + name + ", email=" + email + "]";
-	}
 }
